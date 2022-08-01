9b17e5f320b37179adbe23d91e9ae526504cfee5
Aisha Springer <aishaspringer@gmail.com>
2022-08-01 22:38:01 +0200

    Fix Navbar Spacing

--- script.js
+++ script.js
@@ -2,16 +2,27 @@
 window.onscroll = function() {myFunction()};
 
 // Get the header
-var header = document.getElementById("myHeader");
+var nav = document.getElementById("navbar");
 
 // Get the offset position of the navbar
-var sticky = header.offsetTop;
+var sticky = nav.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
+    nav.classList.add("sticky");
   } else {
-    header.classList.remove("sticky");
+    nav.classList.remove("sticky");
   }
 }
+/* 
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    document.getElementById("nameHeader").style.fontSize = "16px";
+  } else {
+    document.getElementById("nameHeader").style.fontSize = "60px";
+  }
+} */
